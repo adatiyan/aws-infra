9d1b629f5355fb9d623e0c8a39462255995bcbdf
Naiya Adatiya <naiyaadatiya@Naiyas-MacBook-Pro.local>
2023-03-15 19:13:03 -0400

    Egress port modification

--- web-app-module/main.tf
+++ web-app-module/main.tf
@@ -110,9 +110,9 @@ resource "aws_security_group" "app_sg" {
   }
 
   egress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
