3d33d3781f1ad89288bf1b90cd78a93963e57d21
Naiya Adatiya <naiyaadatiya@Naiyas-MacBook-Pro.local>
2023-03-15 17:26:44 -0400

    AMI and ip changed

--- environment/demo/main.tf
+++ environment/demo/main.tf
@@ -21,7 +21,7 @@ module "web_app" {
   vpc_tag_name      = var.vpc_tag_name
   aws_region        = var.aws_region
   aws_subnet_count  = var.aws_subnet_count
-  my_ip             = var.my_ip
+  //my_ip             = var.my_ip
   my_ami            = var.my_ami
   db_storage_size   = var.db_storage_size
   db_instance_class = var.db_instance_class
