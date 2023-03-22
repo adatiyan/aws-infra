67bf13dd4028cee4704b1bfb50905346090fa70d
Naiya Adatiya <naiyaadatiya@Naiyas-MacBook-Pro.local>
2023-03-21 23:19:01 -0400

    Terraform ami and main modified

--- web-app-module/main.tf
+++ web-app-module/main.tf
@@ -1,4 +1,3 @@
-
 # Create a VPC
 resource "aws_vpc" "webapp_vpc" {
   cidr_block = var.cidr_name
@@ -108,13 +107,13 @@ resource "aws_security_group" "app_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allow traffic from all IP addresses
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
 
   tags = {
     Name = "ec2-sg-${timestamp()}" # Set the name tag for the security group
@@ -144,7 +143,7 @@ resource "aws_instance" "webapp_instance" {
   instance_type          = "t2.micro"                     # Set the instance type
   key_name               = "ec2"                          # Set the key pair to use for SSH access
   vpc_security_group_ids = [aws_security_group.app_sg.id] # Set the security group to attach to the instance
-  subnet_id              = local.public_subnet_ids[0]    # Set the ID of the subnet to launch the instance in
+  subnet_id              = local.public_subnet_ids[0]     # Set the ID of the subnet to launch the instance in
   # Enable protection against accidental termination
   disable_api_termination = false
   # Set the root volume size and type
@@ -170,11 +169,21 @@ echo "spring.datasource.username=${aws_db_instance.rds_instance.username}" >> ap
 echo "spring.datasource.password=${aws_db_instance.rds_instance.password}" >> application.properties
 echo "#spring.jpa.properties.hibernate.dialect = org.hibernate.dialect.MySQL5InnoDBDialect" >> application.properties
 echo "spring.jpa.hibernate.ddl-auto=update" >> application.properties
+echo "logging.file.path=/home/ec2-user" >> application.properties
+echo "logging.file.name=/home/ec2-user/csye6225.log" >> application.properties
+echo "publish.metrics=true" >> application.properties
+echo "metrics.server.hostname=localhost" >> application.properties
+echo "metrics.server.port=8125" >> application.properties
 sudo chmod 770 /home/ec2-user/webapp-0.0.1-SNAPSHOT.jar
 sudo cp /tmp/webservice.service /etc/systemd/system
+sudo cp /tmp/cloudwatch-config.json /opt/cloudwatch-config.json
+sudo chmod 770 /opt/cloudwatch-config.json
 sudo chmod 770 /etc/systemd/system/webservice.service
-sudo systemctl start webservice.service
-sudo systemctl enable webservice.service
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/cloudwatch-config.json \
+    -s
 sudo systemctl daemon-reload
 sudo systemctl start webservice.service
 sudo systemctl enable webservice.service
@@ -188,7 +197,7 @@ sudo systemctl enable webservice.service
 resource "random_pet" "rg" {
   keepers = {
     # Generate a new pet name each time we switch to a new profile
-    random_name= "webapp"
+    random_name = "webapp"
   }
 }
 // Create s3 bucket
@@ -230,8 +239,6 @@ resource "aws_s3_bucket_public_access_block" "s3_block" {
   bucket              = aws_s3_bucket.s3b.id
   block_public_acls   = true
   block_public_policy = true
-  ignore_public_acls  = true
-  restrict_public_buckets = true
 }
 resource "aws_iam_policy" "policy" {
   name        = "WebAppS3"
@@ -241,7 +248,7 @@ resource "aws_iam_policy" "policy" {
     "Version" : "2012-10-17"
     "Statement" : [
       {
-        "Action" : ["s3:DeleteObject", "s3:PutObject", "s3:GetObject", "s3:ListAllMyBuckets","s3:ListBucket"]
+        "Action" : ["s3:DeleteObject", "s3:PutObject", "s3:GetObject", "s3:ListAllMyBuckets", "s3:ListBucket"]
         "Effect" : "Allow"
         "Resource" : ["arn:aws:s3:::${aws_s3_bucket.s3b.bucket}",
           "arn:aws:s3:::${aws_s3_bucket.s3b.bucket}/*"]
@@ -275,9 +282,11 @@ resource "aws_iam_policy_attachment" "web-app-s3-attach" {
 
 
 resource "aws_iam_instance_profile" "iam_profile" {
-  name = "iam_profile"
+  name = "i_am_profile"
   role = aws_iam_role.ec2-role.name
 }
+
+
 
 resource "aws_db_subnet_group" "db_subnet_group" {
   description = "Private Subnet group for RDS"
@@ -322,17 +331,24 @@ resource "aws_db_instance" "rds_instance" {
   }
 }
 
-
 data "aws_route53_zone" "hosted_zone" {
-  name = var.domain_name
+  name         = var.domain_name
   private_zone = false
 }
-
-# Create Route53 record
+#  Route53 record
 resource "aws_route53_record" "hosted_zone_record" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-  name =  var.domain_name
-  type = "A"
-  ttl  = "60"
+  name    = var.domain_name
+  type    = "A"
+  ttl     = "60"
   records = [aws_instance.webapp_instance.public_ip]
 }
+
+
+#CloudWatch Policy
+resource "aws_iam_policy_attachment" "web-app-atach-cloudwatch" {
+  name       = "attach-cloudwatch-server-policy-ec2"
+  roles      = [aws_iam_role.ec2-role.name]
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
